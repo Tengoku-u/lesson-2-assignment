978fce338bd8e638747bb6f223b7325f74699f3e
Oye <somethingdeelish@gmail.com>
2021-10-25 15:25:50 +0100

    Initial commit

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float keyLaunchInterval = 1.5f; //time interval between spacebar key press
+    private float nextKeyLaunch = 0; // next keylaunch time 
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, if enough time has passed, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > nextKeyLaunch)
+        {
+            nextKeyLaunch = Time.time + keyLaunchInterval;
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# lesson-2-assignment
+ unity create with code assignment 2
